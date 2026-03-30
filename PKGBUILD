63f7e35a75741da6f2dcf4f39930d329230a7756
Jeicobroot-jrt <jeicobtavarezdiaz@gmail.com>
2026-03-30 18:17:21 -0400

    v0.1.3

--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,6 @@
+#!/bin/bash
 pkgname=libspk
-pkgver=0.1.2
+pkgver=0.1.3
 pkgrel=10
 pkgdesc="Custom commands (functions) and connector of SPK packages for Arch Linux"
 arch=('x86_64')
@@ -7,11 +8,15 @@ url="https://github.com/Jeicobroot-jrt/libspk.git"
 license=('MIT')
 install=SPK.install
 source=("SPK_aliases.sh")
-sha256sums=('a731c497c511f92ea6e27cbac50b844221bfe8523c17a6638dcb99e2415df28b')
+sha256sums=('32db0a4dfee3ecd2a1baffd0ee8283a9bc48698edcf8a0569a40a203311408bb')
 
 package() {
-    install -Dm644 "$srcdir/SPK_aliases.sh" \
-        "$pkgdir/usr/share/spk/SPK_aliases.sh"
+
+install -Dm644 "$srcdir/SPK_aliases.sh" \
+    "$pkgdir/usr/share/spk/SPK_aliases.sh"
+
+install -Dm644 "$srcdir/SPK_aliases.sh" \
+    "$pkgdir/etc/profile.d/SPK_aliases.sh"
 }
 #
 #
--- SPK_aliases.sh
+++ SPK_aliases.sh
@@ -1,7 +1,7 @@
 spk_sysrecovery() {
     echo "==!> WARNING: This will update system and reboot."
     read -p "Continue? (y/N): " confirm
-    [[ $confirm != "y" ]] && return
+    [[ "$confirm" != "y" ]] && return
 
     sudo pacman -Syu
     sudo pacman -Sc
@@ -14,64 +14,17 @@ spk_initialize() {
     sudo mkdir -p /lspbs/libspk-base/
 
     echo "==::> Moving package files..."
-	
+
+    echo "==!> Do you really want to move to a secure directory?"
+    read -p "Continue? (y/N): " confirm
+    [[ "$confirm" != "y" ]] && return
+
     if [[ -f ~/SPK/PKGBUILD && -f ~/SPK/libspk_aliases.sh ]]; then
-    echo "==!>¿really do you want to move to a secure directory?"
-         read -p "Continue? (y/N): " confirm
-    [[ $confirm != "y" ]] && return
-    sudo mv /usr/share/spk/SPK_aliases.sh /lspbs/libspk-base
+        sudo mv /usr/share/spk/SPK_aliases.sh /lspbs/libspk-base
         echo "==+> Files moved successfully"
-        echo "==::> Moving the content of the source to the base"
-	cat <<EOF >> ~/.bashrc
-
-	spk_sysrecovery() {
-	    echo "==!> WARNING: This will update system and reboot."
-	    read -p "Continue? (y/N): " confirm
-	    [[ $confirm != "y" ]] && return
-	
-	    sudo pacman -Syu
-	    sudo pacman -Sc
-	
-	    echo "Rebooting..."
-	    reboot
-	}
-	
-	spk_initialize() {
-	    sudo mkdir -p /lspbs/libspk-base/
-	
-	    echo "==::> Moving package files..."
-		
-	    if [[ -f ~/SPK/PKGBUILD && -f ~/SPK/libspk_aliases.sh ]]; then
-	    echo "==!>¿really do you want to move to a secure directory?"
-	         read -p "Continue? (y/N): " confirm
-	    [[ $confirm != "y" ]] && return
-	    sudo mv /usr/share/spk/SPK_aliases.sh /lspbs/libspk-base
-	        echo "==+> Files moved successfully"
-	        echo "==::> Moving the content of the source to the base"
-		
-	    fi
-	}
-	
-	spk_update() {
-	    sudo pacman -S libspk
-	}
-	
-	spk_libsearch() {
-	    ls /lspbs/libspk-base 2>/dev/null 
-	}
-	
-	spk_connect() {
-	    echo "==::> Searching the S.C.N"
-	
-	    if [ -d "/S.C.N/connect_network" ]; then
-	        ls /S.C.N/connect_network
-	        echo "==+> Connected successfully"
-	    else
-	        echo "==-> Error, you don't have SPKs"
-	    fi
-	}
-
-	EOFq
+    else
+        echo "==-> Required files not found"
+    fi
 }
 
 spk_update() {
@@ -79,7 +32,11 @@ spk_update() {
 }
 
 spk_libsearch() {
-    ls /lspbs/libspk-base 2>/dev/null 
+    if ls /lspbs/libspk-base 2>/dev/null; then
+        echo "==+> Files listed"
+    else
+        echo "==-> No files found"
+    fi
 }
 
 spk_connect() {
