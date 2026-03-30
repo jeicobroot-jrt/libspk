5b176d08b194c70bdcb80f34c99d6fac7f9c8c85
Jeicobroot-jrt <jeicobtavarezdiaz@gmail.com>
2026-03-30 14:40:59 -0400

    ver connect

--- SPK_aliases.sh
+++ SPK_aliases.sh
@@ -13,15 +13,66 @@ spk_sysrecovery() {
 spk_initialize() {
     sudo mkdir -p /lspbs/libspk-base/
 
-    echo "==+> Moving package files..."
-
+    echo "==::> Moving package files..."
+	
     if [[ -f ~/SPK/PKGBUILD && -f ~/SPK/libspk_aliases.sh ]]; then
     echo "==!>¿really do you want to move to a secure directory?"
          read -p "Continue? (y/N): " confirm
     [[ $confirm != "y" ]] && return
     sudo mv /usr/share/spk/SPK_aliases.sh /lspbs/libspk-base
-
         echo "==+> Files moved successfully"
+        echo "==::> Moving the content of the source to the base"
+	cat <<EOF >> ~/.bashrc
+
+	spk_sysrecovery() {
+	    echo "==!> WARNING: This will update system and reboot."
+	    read -p "Continue? (y/N): " confirm
+	    [[ $confirm != "y" ]] && return
+	
+	    sudo pacman -Syu
+	    sudo pacman -Sc
+	
+	    echo "Rebooting..."
+	    reboot
+	}
+	
+	spk_initialize() {
+	    sudo mkdir -p /lspbs/libspk-base/
+	
+	    echo "==::> Moving package files..."
+		
+	    if [[ -f ~/SPK/PKGBUILD && -f ~/SPK/libspk_aliases.sh ]]; then
+	    echo "==!>¿really do you want to move to a secure directory?"
+	         read -p "Continue? (y/N): " confirm
+	    [[ $confirm != "y" ]] && return
+	    sudo mv /usr/share/spk/SPK_aliases.sh /lspbs/libspk-base
+	        echo "==+> Files moved successfully"
+	        echo "==::> Moving the content of the source to the base"
+		
+	    fi
+	}
+	
+	spk_update() {
+	    sudo pacman -S libspk
+	}
+	
+	spk_libsearch() {
+	    ls /lspbs/libspk-base 2>/dev/null 
+	}
+	
+	spk_connect() {
+	    echo "==::> Searching the S.C.N"
+	
+	    if [ -d "/S.C.N/connect_network" ]; then
+	        ls /S.C.N/connect_network
+	        echo "==+> Connected successfully"
+	    else
+	        echo "==-> Error, you don't have SPKs"
+	    fi
+	}
+
+	EOF
+	
     fi
 }
 
